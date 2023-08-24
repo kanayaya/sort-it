81aa6a73a91f20fef71b87410a73af1da944ccea
Kanayaya <kanayaya@mail.ru>
2023-08-24 12:06:25 +0800

    add OrderChecker implementation

---
+++ src/main/java/com/kanayaya/sort_it/read/OrderChecker.java
@@ -0,0 +1,45 @@
+package com.kanayaya.sort_it.read;
+
+import com.kanayaya.sort_it.sort.Dispencer;
+import com.kanayaya.sort_it.sort.Sequencer;
+
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class OrderChecker<T extends Comparable<T>> implements Iterable<T> {
+    private final Iterable<T> source;
+    private final Comparator<T> order;
+
+    public OrderChecker(Iterable<T> source, Comparator<T> comparator) {
+        if ( ! source.iterator().hasNext()) throw new IllegalArgumentException("Source should not be empty");
+        this.source = source;
+        this.order = comparator;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private final Iterator<T> sourceIterator = source.iterator();
+            private final Sequencer<T> sequencer = new Sequencer<>(new Dispencer<T>(order), sourceIterator.next());
+            private T last = null;
+            @Override
+            public boolean hasNext() {
+                if (sourceIterator.hasNext()) return true;
+                boolean result = last == null;
+                last = sequencer.apply(null);
+                return result;
+            }
+
+            @Override
+            public T next() {
+                if (sourceIterator.hasNext()) {
+                    T next = sourceIterator.next();
+                    T result = sequencer.apply(next);
+                    if (result == next) throw new IllegalStateException('"' + result.toString() + "\" is in wrong order");
+                    return result;
+                }
+                return last;
+            }
+        };
+    }
+}
