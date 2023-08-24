5b22da045e1371f657a9ef77df5cf9ef3a8edf93
Kanayaya <kanayaya@mail.ru>
2023-08-24 12:05:52 +0800

    add test for incorrect order

--- src/test/java/ReadTests.java
+++ src/test/java/ReadTests.java
@@ -2,17 +2,12 @@ import com.kanayaya.sort_it.read.OrderChecker;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ReadTests {
     @Test
-    public void testSequenceChecker() {
+    public void testCorrectSequenceChecker() {
         List<String> correctLines = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j");
-        String[] incorrectLines = new String[] {
-                "a", "b", "c", "d", "a", "b", "g", "h", "a", "j"
-        };
 
         OrderChecker<String> correct = new OrderChecker<String>(correctLines, Comparator.naturalOrder());
 
@@ -22,7 +17,35 @@ public class ReadTests {
             Assertions.assertTrue(condition);
             previous = line;
         }
-
-
     }
+
+    @Test
+    public void testIncorrectSequenceThrows() {
+        List<String> incorrectLines = Arrays.asList("a", "b", "c", "d", "a", "b", "g", "h", "a", "j");
+
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            for (String s :
+                    new OrderChecker<String>(incorrectLines, Comparator.naturalOrder())) {
+            }
+        });
+    }
+
+    @Test
+    public void testIncorrectSequenceFiltering() {
+        List<String> incorrectLines = Arrays.asList("a", "b", "c", "d", "a", "b", "g", "h", "a", "j");
+        List<String> results = new ArrayList<>();
+        for (Iterator<String> iterator = new OrderChecker<String>(incorrectLines, Comparator.naturalOrder()).iterator(); iterator.hasNext(); ) {
+            try {
+                String s = iterator.next();
+                results.add(s);
+            } catch (IllegalStateException e) {}
+        }
+        String previous = "";
+        for (String line : results) {
+            boolean condition = line.compareTo(previous) > 0;
+            Assertions.assertTrue(condition);
+            previous = line;
+        }
+    }
+
 }
