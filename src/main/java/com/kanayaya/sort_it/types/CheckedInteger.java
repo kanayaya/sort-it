9fd8220035c7c5063e0e886fcf8f5a27a163ddc9
Kanayaya <kanayaya@mail.ru>
2023-08-27 23:08:47 +0800

    add normal error message

--- src/main/java/com/kanayaya/sort_it/types/CheckedInteger.java
+++ src/main/java/com/kanayaya/sort_it/types/CheckedInteger.java
@@ -3,6 +3,10 @@ package com.kanayaya.sort_it.types;
 public class CheckedInteger implements Convertable<Integer> {
     @Override
     public Integer convert(String candidate) {
-        return Integer.parseInt(candidate);
+        try {
+            return Integer.parseInt(candidate);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Could not convert to integer: " + candidate);
+        }
     }
 }
